8bf895c767c8c4cb5b0683cb22ca387da7d3d8f2
mseifrid <72096848+mseifrid@users.noreply.github.com>
2021-06-14 15:55:23 -0400

    Added annotation to make code easier to understand

--- RS_Base.py
+++ RS_Base.py
@@ -23,7 +23,9 @@ for i in range(len(targets.index)):
     target = targets.at[i, 'pentamer']
 
     # step 1
+    # Initial reaction scale: 0.1 mmol
     scale: float = 0.0001
+    # Calculate StepScore and scale for next step based on reaction yield
     step1, NextStep_scale = rxn.wingSuzuki(
                                            targets.at[i, 'a'],
                                            targets.at[i, 'b'],
@@ -45,6 +47,8 @@ for i in range(len(targets.index)):
     steps.append(step2)
 
     final_scale = NextStep_scale
+    # Calculate RouteScore
+    # Update targets dataframe with information including RouteScore and StepScore details
     targets = gen.Process(
                           targets,
                           i,
@@ -52,4 +56,5 @@ for i in range(len(targets.index)):
                           final_scale
                           )
 
+# Save dataframe of routes with RouteScores
 targets.to_pickle(TARGETS_FILE)
