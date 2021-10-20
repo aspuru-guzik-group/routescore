Added pearson and spearman coeffs to Fig S18. Renumbered figs. Fixed missing ) in RouteScore units.

_Figure_S10.py
@@ -1,45 +1,65 @@
-import os
-import pandas as pd
+#!/usr/bin/env python
+
+import pickle
 import numpy as np
-import matplotlib as mpl
+import pandas as pd
+
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
 
-# Custom style
-mpl.style.use('scientific')
+# distributions of the individual scores
 
-# Define directories
-HERE = os.path.abspath(os.path.dirname(__file__))
-PROP_DIR = os.path.join(HERE, 'Properties')
+# load the full properties
+df_full = pickle.load(open('Properties/full_props.pkl', 'rb'))
 
-# Create colormap
-cm = plt.get_cmap("viridis")
 
-# Load dataframe with all properties
-fp = pd.read_pickle(os.path.join(PROP_DIR, 'full_props.pkl'))
-fp['log_RS'] = np.log10(fp['route_score'])
-fp['log_kR'] = np.log10(fp['fluo_rate_ns'])
+scores = [
+    'naive_score',
+    'route_score',
+    'sa_score',
+    'sc_score',
+    'syba_score',
+    'sr_nn_score',
+]
 
-# Create figure
-fig = plt.figure()
-ax3D = fig.add_subplot(111, projection='3d')
+names = [
+         'Naive score [$ mol$^{-1}$]',
+         'RouteScore',
+         'SAscore',
+         'SCscore',
+         'SYBAscore',
+         'RAscore-NN',
+    ]
 
-# 3D scatter plot
-ax = ax3D.scatter3D(fp['log_RS'], fp['overlap'], fp['log_kR'], c=cm(fp['fluo_peak_1']), marker='o')
+fig, axes = plt.subplots(1, 6, figsize=(15, 3.5))
+axes = axes.flatten()
 
-# Colorbar
-fig.colorbar(ax,
-             ax=ax3D,
-             label='Peak score',
-             pad=0.1,
-             shrink=0.42,
-             aspect=10,
-             anchor=(0.0, 1.0),
-             panchor=(1.0, 1.0),
-             )
+for ix, (ax, score, name) in enumerate(zip(axes, scores, names)):
+    if score == 'route_score':
+        sns.distplot(
+            #np.log10(df_full[score]),
+            df_full[score],
+            kde=False,
+            ax=ax,
+        )
+    else:
+        sns.distplot(
+            df_full[score],
+            kde=False,
+            ax=ax,
+        )
 
-ax3D.set_xlabel('\nlog(RouteScore)\n$(h \cdot \$ \cdot g \cdot (mol \  target)^{-1}$)')
-ax3D.set_xlim(3.75, 8.25)
-ax3D.set_ylabel('Spectral overlap')
-ax3D.set_zlabel('log(Fluorescence rate) ($ns^{-1}$)')
+    if ix == 0:
+        ax.set_xticklabels(ax.get_xticks(), rotation=45)
+        ax.set_xlabel('Naive score $(\$ \cdot mol^{-1})$', fontsize=12)
+        ax.set_ylabel('Absolute frequency', fontsize=12)
+    elif ix == 1:
+        ax.set_xlabel('RouteScore\n$(h \cdot \$ \cdot g \cdot (mol \  target)^{-1}$)', fontsize=12)
+    else:
+        ax.set_xlabel(name, fontsize=12)
+
+
+
+plt.tight_layout()
+plt.savefig('Figure_S10.png', dpi=300)
 plt.show()

_Figure_S14.py
@@ -1,38 +1,45 @@
-#!/usr/bin/env python
-
-import numpy as np
+import os
 import pandas as pd
-
+import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 
+# Custom style
+mpl.style.use('scientific')
 
-# load random runs
-df_rand = pd.read_pickle('Optimization/runs/frac_found_random.pkl')
+# Define directories
+HERE = os.path.abspath(os.path.dirname(__file__))
+PROP_DIR = os.path.join(HERE, 'Properties')
 
-# load gryffin runs
-df_gryf = pd.read_pickle('Optimization/runs/frac_found_gryffin.pkl')
+# Create colormap
+cm = plt.get_cmap("viridis")
 
+# Load dataframe with all properties
+fp = pd.read_pickle(os.path.join(PROP_DIR, 'full_props.pkl'))
+fp['log_RS'] = np.log10(fp['route_score'])
+fp['log_kR'] = np.log10(fp['fluo_rate_ns'])
 
-# make plot
+# Create figure
+fig = plt.figure()
+ax3D = fig.add_subplot(111, projection='3d')
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 4))
+# 3D scatter plot
+ax = ax3D.scatter3D(fp['log_RS'], fp['overlap'], fp['log_kR'], c=cm(fp['fluo_peak_1']), marker='o')
 
-sns.lineplot(data=df_rand, x='iter', y='per',
-             lw=3,
-             label='Random Sampling',
-             ax=ax)
-sns.lineplot(data=df_gryf, x='iter', y='per',
-             lw=3,
-             label='Gryffin + Chimera',
-             ax=ax)
+# Colorbar
+fig.colorbar(ax,
+             ax=ax3D,
+             label='Peak score',
+             pad=0.1,
+             shrink=0.42,
+             aspect=10,
+             anchor=(0.0, 1.0),
+             panchor=(1.0, 1.0),
+             )
 
-ax.legend(loc='upper left', fontsize=15)
-ax.set_xlabel('Number of evaluations', fontsize=15)
-ax.set_ylabel('Acceptable\n molecules (%)', fontsize=15)
-ax.set_xlim(0, 500)
-ax.set_ylim(0., 41.)
-ax.tick_params(labelsize=15)
-
-plt.tight_layout()
+ax3D.set_xlabel('\nlog(RouteScore)\n$(h \cdot \$ \cdot g \cdot (mol \  target)^{-1}$)')
+ax3D.set_xlim(3.75, 8.25)
+ax3D.set_ylabel('Spectral overlap')
+ax3D.set_zlabel('log(Fluorescence rate) ($ns^{-1}$)')
 plt.show()

_Figure_S17.py
@@ -1,64 +1,45 @@
 #!/usr/bin/env python
 
-import pickle
 import numpy as np
 import pandas as pd
+
+import itertools
+from scipy.stats import pearsonr, spearmanr
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# distributions of the individual scores
 
-# load the full properties
-df_full = pickle.load(open('Properties/full_props.pkl', 'rb'))
+# load the data
+target_df = pd.read_pickle('Properties/full_props.pkl')
 
 
-scores = [
-    'naive_score',
-    'route_score',
-    'sa_score',
-    'sc_score',
-    'syba_score',
-    'sr_nn_score',
-]
+# make correlations plot
+data_types = ['fluo_peak_1', 'route_score', 'overlap', 'fluo_rate_ns']
+labels     = {'fluo_peak_1': 'Peak score',
+              'route_score': 'RouteScore\n$(h \cdot \$ \cdot g \cdot (mol \  target)^{-1}$)',
+              'overlap': 'Spectral overlap',
+              'fluo_rate_ns': 'Fluorescence \nrate (ns$^-1$)'}
+combs = list(itertools.combinations(data_types, 2))
 
-names = [
-         'Naive score [$ mol$^{-1}$]',
-         'RouteScore',
-         'SAscore',
-         'SCscore',
-         'SYBAscore',
-         'RAscore-NN',
-    ]
-
-fig, axes = plt.subplots(1, 6, figsize=(15, 3.5))
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 6))
 axes = axes.flatten()
+pal = sns.color_palette('rocket')
 
-for ix, (ax, score, name) in enumerate(zip(axes, scores, names)):
-    if score == 'route_score':
-        sns.distplot(
-            #np.log10(df_full[score]),
-            df_full[score],
-            kde=False,
-            ax=ax,
-        )
-    else:
-        sns.distplot(
-            df_full[score],
-            kde=False,
-            ax=ax,
-        )
+for ix, comb in enumerate(combs):
 
-    if ix == 0:
-        ax.set_xticklabels(ax.get_xticks(), rotation=45)
-        ax.set_xlabel('Naive score $(\$ \cdot mol^{-1})$', fontsize=12)
-        ax.set_ylabel('Absolute frequency', fontsize=12)
-    elif ix == 1:
-        ax.set_xlabel('RouteScore\n$(h \cdot \$ \cdot g \cdot (mol \  target)^{-1}$)', fontsize=12)
-    else:
-        ax.set_xlabel(name, fontsize=12)
+    sns.scatterplot(target_df[comb[0]], target_df[comb[1]],
+                    c=pal[ix], alpha=0.8, ax=axes[ix])
 
+    spear = spearmanr(target_df[comb[0]], target_df[comb[1]])
+    pear = pearsonr(target_df[comb[0]], target_df[comb[1]])
 
+    axes[ix].set_title(f'pearson = {round(pear[0], 2)}\nspearman = {round(spear[0], 2)}', fontsize=10)
+
+    axes[ix].set_xlabel(labels[comb[0]], fontsize=12)
+    axes[ix].set_ylabel(labels[comb[1]], fontsize=12)
+
+plt.ticklabel_format(useOffset=False)
 
 plt.tight_layout()
 plt.savefig('Figure_S17.png', dpi=300)

_Figure_S18.py
@@ -1,36 +1,54 @@
 #!/usr/bin/env python
 
-import pickle
 import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# distributions of the individual scores
+# absolute tolerances for chimera
+absolutes = np.array([0.67, 1080000, 0.2, 0.15848931924611134])
 
-# load the full properties
-df_full = pickle.load(open('Properties/full_props.pkl', 'rb'))
+# load in gryffin runs with Naive score as objective
+df_naive = pd.read_pickle('Optimization/runs/gryffin_runs_naive.pkl')
 
+# make the plot
 
-# pairwise correlations of the naive score and SA, SC, SYBA, and RAscore-NN
-fig, axes = plt.subplots(1, 4, figsize=(12, 3.5), sharey=True)
-axes = axes.flatten()
+fig, axes = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(8, 10))
 
-scores = ['sa_score', 'sc_score', 'syba_score', 'sr_nn_score']
-names = ['SAscore', 'SCscore', 'SYBAscore', 'RAscore-NN']
+sns.lineplot(x='eval', y='peak_score', data=df_naive, ax=axes[0], label='Naive Score Included')
+axes[0].axhline(absolutes[0], ls='--', linewidth=2, c='k', alpha=0.6)
+axes[0].fill_between(df_naive['eval'], absolutes[0], np.amin(df_naive['peak_score']), color='#8C9196', alpha=0.25)
+axes[0].set_ylim(0.25, 0.9)
+axes[0].set_ylabel('Peak score ', fontsize=15)
+axes[0].tick_params(labelsize=13)
+axes[0].legend(loc='lower right', ncol=1, fontsize=15)
 
-for ix, (ax, score, name) in enumerate(zip(axes, scores, names)):
+sns.lineplot(x='eval', y='naive_score', data=df_naive, ax=axes[1])
+axes[1].set_yscale('log')
+axes[1].axhline(absolutes[1], ls='--', linewidth=2, c='k', alpha=0.6)
+axes[1].fill_between(df_naive['eval'], absolutes[1], np.amax(df_naive['naive_score']), color='#8C9196', alpha=0.25)
+axes[1].set_ylim(np.amin(df_naive['naive_score']), np.amax(df_naive['naive_score']))
+axes[1].set_ylabel('Naive score \n$( \$ \cdot mol^{-1}$)', fontsize=15)
+axes[1].tick_params(labelsize=13)
 
-    sns.scatterplot(
-            df_full[score],
-            df_full['naive_score'],
-            ax=ax,
-        )
-    ax.set_xlabel(name, fontsize=12)
-axes[0].set_ylabel('Naive score $( \$ \cdot mol^{-1})$', fontsize=12)
+sns.lineplot(x='eval', y='spectral_overlap', data=df_naive, ax=axes[2])
+axes[2].axhline(absolutes[2], ls='--', linewidth=2, c='k', alpha=0.6)
+axes[2].fill_between(df_naive['eval'], absolutes[2], np.amax(df_naive['spectral_overlap']), color='#8C9196', alpha=0.25)
+axes[2].set_ylim(0., 0.3)
+axes[2].set_ylabel('Spectral \noverlap', fontsize=15)
+axes[2].tick_params(labelsize=13)
 
+sns.lineplot(x='eval', y='fluo_rate', data=df_naive, ax=axes[3])
+axes[3].axhline(absolutes[3], ls='--', linewidth=2, c='k', alpha=0.6)
+axes[3].fill_between(df_naive['eval'], absolutes[3], np.amin(df_naive['fluo_rate']), color='#8C9196', alpha=0.25)
+axes[3].set_ylim(0., 0.6)
+axes[3].set_ylabel('Fluorescence \nrate (ns$^{-1}$)', fontsize=15)
+axes[3].tick_params(labelsize=13)
+axes[3].set_xlabel('Number of evaluations', fontsize=15)
 
+for ax in axes:
+    ax.set_xlim(0, 500)
 
 plt.tight_layout()
 plt.savefig('Figure_S18.png', dpi=300)