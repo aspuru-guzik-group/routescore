759f2587363fd6ad09361296520db374b703e63c
mseifrid <72096848+mseifrid@users.noreply.github.com>
2021-11-03 19:20:29 -0400

    Updated script for creating all figures (`all_figs.py`).

--- all_figs.py
+++ all_figs.py
@@ -3,12 +3,17 @@ import _Figure_2
 # from _Modafinil import _Figures_4_S3
 import _Figure_6
 import _Figure_7a
-# import _Figure_7be
-import _Figures_8b_S15b
+import _Figure_7be
+import _Figures_8b_S20b
 
 # Make figures for SI
 # import _Figure_S2
 import Figures_S7_S8
+import _Figure_S9
 import _Figure_S10
-import _Figure_S13
+import _Figure_S11
+import _Figure_S12
 import _Figure_S14
+import _Figure_S17
+import _Figure_S18
+import _Figure_S19
